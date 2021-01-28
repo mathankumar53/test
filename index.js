897283a32c17f132b5a63e1eea31acd8979072c4
mathankumar <mathankumarmca92@gmail.com>
2021-01-28 14:11:32 +0530

    change

---
+++ index.js
@@ -0,0 +1,66 @@
+alert("smart mathan");
+console.log("hi mathan");
+
+// function result(mark) {
+
+//     if (mark > 34 && mark < 60) {
+//         console.log("your pass only");
+//     } else if (mark > 59 && mark < 80) {
+//         console.log("first class mark");
+//     } else if (mark > 79 && mark < 100) {
+//         console.log("your getting distition");
+
+//     } else if (mark > 99) {
+//         console.log("your giving more then 100 marks please check it");
+//     } else {
+
+//         console.log("your getting fail marks");
+//     }
+
+// }
+// result(60);
+
+
+// function add(symbol) {
+
+//     switch (symbol) {
+//         case "+":
+//             console.log("addition");
+//             break;
+//         case "-":
+//             console.log("subtraction");
+//             break;
+//         case "*":
+//             console.log("multiplication");
+//             break;
+//         case "/":
+//             console.log("divition");
+//             break;
+//         default:
+//             console.log("give correct symbol");
+//             break;
+//     }
+
+
+// }
+
+// add("+");
+
+
+
+
+
+function loop(value) {
+
+
+    for (let a = 1; a <= 50; a++) {
+        console.log("mathan Lakshmanan");
+        confirm("ok");
+
+    }
+
+
+
+}
+
+loop(10);
